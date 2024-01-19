dad7c901c15b9f5c3f569f1dc7e5db41ffa57329
Npx <cheyprivado@gmail.com>
2024-01-18 21:53:33 -0300

    💄 Atualizando estrutura e estilo carc

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export function App() {
   return (
     <>
       <div className="font-mono h-screen w-screen bg-slate-900 text-slate-50 flex justify-center items-center flex-col">
-        <header className="w-3/5">
+        <header className="w-3/5 mb-8">
           <div className="flex justify-between items-center mb-6">
             <h1 className="font-bold text-2xl">devfinder</h1>
             {/* <button className="flex items-center gap-2 font-bold uppercase tracking-widest text-slate-500 hover:text-slate-50 ease-in duration-200">
@@ -29,7 +29,15 @@ export function App() {
           </div>
         </header>
 
-        <main className="w-3/5 flex items-center"></main>
+        {/* <main className="w-3/5 flex items-center">
+          <div className="w-full bg-blue-950 p-9 rounded-md">
+            <img
+              className="rounded-full w-32"
+              src="https://cdn.pixabay.com/photo/2018/11/13/21/43/avatar-3814049_960_720.png"
+              alt="Avatar"
+            />
+          </div>
+        </main> */}
       </div>
     </>
   );
